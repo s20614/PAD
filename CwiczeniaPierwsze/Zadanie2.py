598d9026211ae89aad5a824c447a5e9ba4619666
s20614 <s20614@pjwstk.edu.pl>
2023-10-27 12:22:09 +0200

    Added description for Zadanie2

--- CwiczeniaPierwsze/Zadanie2.py
+++ CwiczeniaPierwsze/Zadanie2.py
@@ -1,4 +1,4 @@
-# Napisz funkcję o nazwie unique_list, która pobiera listę i zwraca listę zawierającą tylko unikalne elementy danych wejściowych.
-
+# Napisz funkcję o nazwie unique_list, która pobiera listę i zwraca listę zawierającą tylko unikalne elementy danych
+# wejściowych.
 def unique_list(list_with_duplicates):
     return list(set(list_with_duplicates))
